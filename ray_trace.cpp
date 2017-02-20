5c5d5ce91a1756ad796263f98b2d138384174791
Catherine Taylor <cat.taylor.taylor@btinternet.com>
2017-02-20 22:46:54 +0000

    Refactor code a bit.

--- ray_trace.cpp
+++ ray_trace.cpp
@@ -8,18 +8,9 @@
 using namespace std;
 
 #define PI 3.141592654f
-
-sphere::sphere(int sphere_x, int sphere_y, int sphere_z, int sphere_radius, int sphere_colour){
-    centre_x=sphere_x;
-    centre_y=sphere_y;
-    centre_z=sphere_z;
-    radius=sphere_radius;
-    if (sphere_colour ==RED){
-        colour_x=255;
-        colour_y=0;
-        colour_z=0;
-    }
-}
+const int RED[] = {255,0,0};
+const int BLUE[] ={0,255,0};
+const int GREEN[] = {0,0,255};
 
 vector3::vector3(float x, float y, float z){
     x_val=x;
@@ -41,8 +32,8 @@ float dotproduct(vector3 vec1, vector3 vec2){
 vector3 crossproduct(vector3 u, vector3 v){
     float i,j,k;
     i = u.get_y()*v.get_z() - u.get_z()*v.get_y();
-     j = u.get_z()*v.get_x() - u.get_x()*v.get_z();
-      k = u.get_x()*v.get_y() - u.get_y()*v.get_x();
+    j = u.get_z()*v.get_x() - u.get_x()*v.get_z();
+    k = u.get_x()*v.get_y() - u.get_y()*v.get_x();
     vector3 vec(i, j, k);
     return vec;
 }
@@ -68,11 +59,45 @@ void vector3::normalize(void){
     y_val /= sum;
     z_val /=sum;
 }
+sphere::sphere(int sphere_x, int sphere_y, int sphere_z, int sphere_radius,  const int* sphere_colour){
+    centre_x = sphere_x;
+    centre_y=sphere_y;
+    centre_z=sphere_z;
+    radius=sphere_radius;
+    colour_x=sphere_colour[0];
+    colour_y=sphere_colour[1];
+    colour_z= sphere_colour[2];
+}
+vector3 sphere::get_colour(void){
+    vector3 colour(colour_x, colour_y, colour_z);
+    return colour;
+}
+int sphere::get_radius(void){
+    return radius;
+}
+int sphere::get_centre_x(void){
+    return centre_x;
+}
+int sphere::get_centre_y(void){
+    return centre_y;
+}
+int sphere::get_centre_z(void){
+    return centre_z;
+}
+vector3 sphere::find_normal(vector3 point){     
+    vector3 centre(centre_x, centre_y, centre_z);
+    vector3 norm = vec_add(point, vec_scal_mult(-1,centre));
+    vector3 normal = vec_scal_mult(2,norm);
+    normal.normalize();
+    return normal;
+}
+
 void vector3::setValue(int x, int y, int z){
     x_val=x;
     y_val=y;
     z_val=z;
 }
+
 scene::scene(int x, int y, int fov, int d){
     x_res = x;
     y_res= y;
@@ -92,6 +117,20 @@ float scene::get_width(void){
 float scene::get_height(void){
     return height;
 }
+float Sphere_ray_intersection(int radius, vector3 centre, vector3 ray_point, vector3 ray_direction){
+    float a,b,c, det ;
+    a = dotproduct(ray_direction,ray_direction);
+    b=2*dotproduct(ray_direction,ray_point);
+    c=dotproduct(ray_point,ray_point)-radius*radius;
+    det = b*b - 4*a*c;
+    if (det >= 0){     
+        float  t1,t2;
+        t1 = (-1*b -sqrt(det))/(2*a);
+        t2 = (-1*b +sqrt(det))/(2*a);
+        return (t1>t2)*t2 + (t2>t1)*t1;
+    }
+    return 0;
+ }
 
 int main(){
 
@@ -101,139 +140,79 @@ int main(){
     vector3 lookup(0,1,-5);
     vector3 lookat(0,0,1);
     vector3 light(4,3,-5);
-    vector3 centre(0,0,0);
-    float d = 3, DiffuseCoeff=0.6, AmbientCoeff = 0.1, n=600,  SpecularCoeff=0.7;
+    vector3 centre(sphere1.get_centre_x(),sphere1.get_centre_y(),sphere1.get_centre_z());
+    float d = 3, DiffuseCoeff=0.6, AmbientCoeff = 0.1, n=600,  SpecularCoeff=0.7*255;
     scene myscene(1000,1000,90,3);
     
 //set up eye coord system
-
-vector3 w = vec_add(eye, vec_scal_mult(-1, lookat));
-w.normalize();
-  vector3 u = crossproduct(lookup, w);
-  u.normalize();
-  vector3 v = crossproduct(w,u);
+    vector3 w = vec_add(eye, vec_scal_mult(-1, lookat));
+    w.normalize();
+    vector3 u = crossproduct(lookup, w);
+    u.normalize();
+    vector3 v = crossproduct(w,u); 
+    vector3 C = vec_add(eye,vec_scal_mult(-d,w)); 
+    float Lx,Ly,Lz;  
+    Lx = C.get_x()+u.get_x()*myscene.get_width()/2+v.get_x()*myscene.get_height()/2;
+    Ly = C.get_y()+u.get_y()*myscene.get_width()/2+v.get_y()*myscene.get_height()/2;
+    Lz = C.get_z()+u.get_z()*myscene.get_width()/2+v.get_z()*myscene.get_height()/2;
+    vector3 L(Lx,Ly,Lz);
   
-
- vector3 C = vec_add(eye,vec_scal_mult(-d,w));
- 
-  float Lx,Ly,Lz;
-  
-  Lx = C.get_x()+u.get_x()*myscene.get_width()/2+v.get_x()*myscene.get_height()/2;
-  Ly = C.get_y()+u.get_y()*myscene.get_width()/2+v.get_y()*myscene.get_height()/2;
-  Lz = C.get_z()+u.get_z()*myscene.get_width()/2+v.get_z()*myscene.get_height()/2;
- 
-   vector3 L(Lx,Ly,Lz);
-  
-unsigned char *img = new unsigned char[3*myscene.get_x_res()*myscene.get_y_res()];
-unsigned char *img2 = new unsigned char[myscene.get_x_res()*myscene.get_y_res()];
-cout<<myscene.get_x_res()<<"\n";
-
-for (int x = 0; x<3*myscene.get_x_res()*myscene.get_y_res(); x+=3){
-    int i, j;
-    i=(x/(3))%(myscene.get_x_res());
-    j=(x/(3))/(myscene.get_x_res());
-   
-    
-    // cout<<"i "<<i<<" ";
-    // cout<<"j "<<j<<"\n";    
-      float sx, sy,sz;
+    unsigned char *img = new unsigned char[3*myscene.get_x_res()*myscene.get_y_res()];
+    float sx, sy,sz;
+    double D,DD,Red_term, Green_term, Blue_term;
+    for (int x = 0; x<3*myscene.get_x_res()*myscene.get_y_res(); x+=3){
+        int i, j;
+        i=(x/(3))%(myscene.get_x_res());
+        j=(x/(3))/(myscene.get_x_res());
         sx = L.get_x() - u.get_x()*(myscene.get_width())/((float)myscene.get_x_res())*i - v.get_x()*(myscene.get_height())/((float)myscene.get_y_res())*j;
         sy = L.get_y() - u.get_y()*(myscene.get_width())/((float)myscene.get_x_res())*i - v.get_y()*(myscene.get_height())/((float)myscene.get_y_res())*j;
         sz = L.get_z()- u.get_z()*(myscene.get_width())/((float)myscene.get_x_res())*i - v.get_z()*(myscene.get_height())/((float)myscene.get_y_res())*j;
-        // cout<<"sx "<<sx<<" ";
-        // cout<<"sy "<<sy<<" ";
-        // cout<<"sz "<<sz<<"\n";
-
- vector3 s(sx,sy,sz);
- vector3 d(s.get_x()-eye.get_x(),s.get_y()-eye.get_y(),s.get_z()-eye.get_z());
+        vector3 s(sx,sy,sz);
+        vector3 d(s.get_x()-eye.get_x(),s.get_y()-eye.get_y(),s.get_z()-eye.get_z());
         d.normalize();
 
-float a,b,c, det ;
- a = dotproduct(d,d);
- b=2*dotproduct(d,eye);
- c=dotproduct(eye,eye)-1;
-
-det = b*b - 4*a*c;
-if (det >= 0){
-     
-    float t, t1,t2;
-    t1 = (-1*b -sqrt(det))/(2*a);
-     t2 = (-1*b +sqrt(det))/(2*a);
-     if(t1>t2){
-         t=t2;
-     }
-     else{
-         t=t1;
-     }
-     
-    vector3 point = vec_add(eye, vec_scal_mult(t,d)); 
-  
-    vector3 norm = vec_add(point, vec_scal_mult(-1,centre));
-    vector3 normal = vec_scal_mult(2,norm);
-    normal.normalize();
-     
-  
-   
-
-  vector3 l = vec_add(light, vec_scal_mult(-1,point));
-        l.normalize();
-        
-
-   double D,DD;
-    if (dotproduct(normal,l)>0){
-        D = dotproduct(normal,l);
+        float t = Sphere_ray_intersection(sphere1.get_radius(), centre, eye, d);
+        if (t != 0){
+            vector3 point = vec_add(eye, vec_scal_mult(t,d)); 
+            vector3 normal=sphere1.find_normal(point);   
+            vector3 l = vec_add(light, vec_scal_mult(-1,point));
+            l.normalize();    
+            if (dotproduct(normal,l)>0){
+                D = dotproduct(normal,l);
+            }
+            else{
+                D=0;
+            }
+            vector3 H =vec_add(l, vec_scal_mult(-1,d));
+            H.normalize();
+            if (dotproduct(normal, H)<0){
+                DD=0;
+            }
+            else{
+                DD=dotproduct(normal, H);
+            }   
+            Red_term = (DiffuseCoeff*D+AmbientCoeff)*(sphere1.get_colour()).get_x()+pow(DD,n)*SpecularCoeff;
+            Blue_term =(DiffuseCoeff*D+AmbientCoeff)*(sphere1.get_colour()).get_y()+pow(DD,n)*SpecularCoeff;
+            Green_term =(DiffuseCoeff*D+AmbientCoeff)*(sphere1.get_colour()).get_z()+pow(DD,n)*SpecularCoeff;
+            if (Red_term > 255){
+                Red_term =255;
+            }
+            if (Blue_term>255){
+                Blue_term=255;
+            }
+            if (Green_term >255){
+                Green_term = 255;
+            }
+            img[x] = Red_term;
+            img[x+1]=Green_term;
+            img[x+2]=Blue_term;    
         }
-    else{
-        D=0;
+        else{
+            img[x]=0;
+            img[x+1]=0;
+            img[x+2]=0; 
+        }
     }
-     vector3 H =vec_add(l, vec_scal_mult(-1,d));
-         H.normalize();
-    // float r = 2*dotproduct(l, normal);
-
-    // vector3 R = vec_add(vec_scal_mult(r, normal), vec_scal_mult(-1, l));
-    // R.normalize();
-
-    //cout<<"pointz "<<point.get_z()<<"\n";
-    // vector3 V = vec_scal_mult(-1,d);
-    // V.normalize();
-    // cout<<"Vx "<<V.get_x()<<" ";
-    // cout<<"Vy "<<V.get_y()<<" ";
-    // cout<<"Vz "<<V.get_z()<<"\n";
-         if (dotproduct(normal, H)<0){
-             DD=0;
-         }
-         else{
-             DD=dotproduct(normal, H);
-        // cout<<"DD "<<DD<<"\n";
-                  }
-  
-
-    double Red_term, Green_term, Blue_term;
-    Red_term = 255*DiffuseCoeff*D+255*AmbientCoeff+255*pow(DD,n)*SpecularCoeff;
-    Blue_term =255*pow(DD,n)*SpecularCoeff;
-    Green_term =255*pow(DD,n)*SpecularCoeff;
-    //cout<<"red "<<Red_term<<"\n";
-    if (Red_term > 255){
-        Red_term =255;
-    }
-    if (Blue_term>255){
-        Blue_term=255;
-    }
-    if (Green_term >255){
-        Green_term = 255;
-    }
-    img[x] = Red_term;
-    img[x+1]=Green_term;
-    img[x+2]=Blue_term;
-    
-}
-else{
-    img[x]=0;
-    img[x+1]=0;
-img[x+2]=0;
- 
-}
-}
 
     ofstream my_image;
     my_image.open("test.ppm", ios::out| ios::binary);
@@ -246,8 +225,5 @@ img[x+2]=0;
         
     }
     my_image.close();
-
-
-
     return 0;
 }
--- ray_trace.hpp
+++ ray_trace.hpp
@@ -2,29 +2,6 @@
 #define ray_trace_hpp
 #include<iostream>
 
-#define RED 0
-#define GREEN 1
-#define BLUE 2
-
-
-class sphere{
-    public:
-        sphere(int sphere_x, int sphere_y, int sphere_z, int sphere_radius, int sphere_colour);
-        int get_x_coord(void);
-        int get_y_coord(void);
-        int get_z_coord(void);
-        int get_radius(void);
-    private:
-        int centre_x;
-        int centre_y;
-        int centre_z;
-        int radius;
-        int colour_x;
-        int colour_y;
-        int colour_z;
-};
-
-
 
 class vector3{
     public:
@@ -40,6 +17,30 @@ class vector3{
       float y_val;
       float z_val;
 };
+class sphere{
+    public:
+        sphere(int sphere_x, int sphere_y, int sphere_z, int sphere_radius, const int* sphere_colour);
+        int get_x_coord(void);
+        int get_y_coord(void);
+        int get_z_coord(void);
+        int get_radius(void);
+       int get_centre_x(void);
+       int get_centre_y(void);
+       int get_centre_z(void);
+       vector3 get_colour(void);
+       vector3 find_normal(vector3 point);
+    private:
+        int centre_x;
+        int centre_y;
+        int centre_z;
+        int radius;;
+        int colour_x;
+        int colour_y;
+        int colour_z;
+};
+
+
+
 class scene{
     public:
     scene(int x, int y, int fov, int d);
